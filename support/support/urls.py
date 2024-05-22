e6c83d1b121fdbe2e95fd099aaca345f7ce50803
Giorgio <puncho_giorgio@inbox.lv>
2024-05-22 16:44:32 +0300

    Homework 19 files are added

---
+++ support/support/urls.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.urls import path
+from issues.api import (
+    IssuesAPI,
+    IssuesRetrieveUpdateDeleteAPI,
+    issues_close,
+    issues_take,
+    messages_api_dispatcher,
+)
+from rest_framework_simplejwt.views import TokenObtainPairView  # noqa
+from rest_framework_simplejwt.views import token_obtain_pair  # noqa
+from users.api import UserListCreateAPI
+
+urlpatterns = [
+    # admin
+    path("admin/", admin.site.urls),
+    # users
+    path("users/", UserListCreateAPI.as_view()),
+    # issues
+    path("issues/", IssuesAPI.as_view()),
+    path("issues/<int:id>", IssuesRetrieveUpdateDeleteAPI.as_view()),
+    path("issues/<int:id>/close", issues_close),
+    path("issues/<int:id>/take", issues_take),
+    # messages
+    path("issues/<int:issue_id>/messages", messages_api_dispatcher),
+    # Authentification
+    # path("auth/token/", token_obtain_pair),
+    path("auth/token/", TokenObtainPairView.as_view()),
+]
